507c83f0fb8a02a1ed9be164888b02b0f4b06c0b
Paul Kao <paulkao94@gmail.com>
2017-09-19 11:08:41 -0700

    FInito

---
+++ MVClab/Controllers/FilesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace MVClab.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFile"));
+            ViewBag.fileArray = files;
+            return View();
+        }
+
+        public ActionResult Content(string id)
+        {
+            var newPath = Server.MapPath($"~/TextFile/{id}.txt");
+            ViewBag.content = System.IO.File.ReadAllText(newPath);
+
+            return View();
+        }
+    }
+}
